a1ae4d88947b67d1e6eb64ee81869619a26e0e4e
helloWorld141 <dn.kyle141@gmail.com>
2016-11-04 00:46:14 +0800

    Staff done

--- RRPSS/src/Staff.java
+++ RRPSS/src/Staff.java
@@ -1,4 +1,28 @@
 
 public class Staff {
-
+	private String name;
+	private String gender;
+	private int staffID;
+	private String role;
+	
+	Staff(String name, String gender, int ID, String role){
+		this.name = name;
+		this.gender = gender;
+		this.staffID = ID;
+		this.role = role;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	public String getGender(){
+		return this.gender;
+	}
+	
+	public int getStaffID(){
+		return this.staffID;
+	}
+	public String getRole(){
+		return this.role;
+	}
 }
